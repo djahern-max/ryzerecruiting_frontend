cd10d2a19c9fd648bad818b86ae726b7024b15eb
djahern-max <danielaherniv@gmail.com>
2026-03-16 08:40:12 -0400

    adding pulsing image to logo

--- src/pages/ChatPage.jsx
+++ src/pages/ChatPage.jsx
@@ -174,7 +174,7 @@ function MeetingCard({ meeting }) {
 function TypingIndicator({ statusMsg }) {
     return (
         <div className={styles.messageRow}>
-            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 375 375" width={28} height={28} style={{ flexShrink: 0, marginTop: 2 }}>
+            <svg className={styles.logoPulsing} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 375 375" width={28} height={28} style={{ flexShrink: 0, marginTop: 2 }}>
                 <path fill="#0a66c2" d="M 186.078125 19.484375 L 0.367188 341.148438 L 180.234375 341.148438 L 229.054688 256.585938 L 201.605469 215.015625 L 190.46875 234.308594 L 154.511719 296.59375 L 77.539062 296.59375 L 186.394531 108.039062 L 296.730469 295.972656 L 243.730469 295.972656 L 221.453125 340.527344 L 374.554688 340.527344 Z" />
             </svg>
             <div className={`${styles.bubble} ${styles.bubbleAI}`}>
